5815bd1ad042ad5307d67d53a960f29f9dbbfd5b
samuele <samuelebompani@gmail.com>
2021-02-04 20:50:39 +0100

    added class app

--- index.php
+++ index.php
@@ -19,6 +19,23 @@
         </header>
 
         <main>
+            <div class="wrapper">
+                <div class="app">
+                    <h2>Easy Counter</h2>
+                    <p>
+                        If you need to count something, here's your app. Easy to use, easy to implement.<br>
+                        Download available <a href="#">here</a> and in the <a href="https://play.google.com/store/apps/details?id=com.easy.counter">Play Store</a><br><br>
+                        <img 
+                        src="./greedyspaghetti-site/media/easy_counter_logo512.png"
+                        width="64" height="64">
+                    </p>
+                </div>
+                <div class="app">
+                    <h2>Football Headers</h2>
+                    <p>
+                        I'm still developing this game
+                    </p>
+            </div>
         </main>
 
         <footer>

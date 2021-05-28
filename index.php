2b8083bad20d084f1260dbd27c4819a2e6565966
samuele <samuelebompani@gmail.com>
2021-05-28 12:36:40 +0200

    css changes for mobile compatibility. incoplete

--- index.php
+++ index.php
@@ -1,7 +1,50 @@
 <!DOCTYPE html>
 <html>
-    <?php
-        header("Location: menu.php");
-        exit();
-    ?>
+    <head>
+        <title>Greedy Spaghetti</title>
+        <?php include("utils/header.php"); ?>
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php include("utils/navigation.php"); ?>
+        <div class="row">
+            <div class="block-first col-6" onclick="window.location.href='#'">
+                <h2 class="block-title">Bip39</h2>
+                <p class="col-6">A tool to generate unsecure mnemonic for crypto wallets</p>
+                <div class="col-6">
+                    <button onclick="window.location.href='#'">Here</button>
+                </div>
+            </div>
+
+            <div class="block-second col-6">
+                <h2 class="block-title">Easy Counter</h2>
+                <p class="col-6">
+                    If you need to count something, here's your app. Easy to use, easy to implement.<br>
+                    Download available in the <a href="https://play.google.com/store/apps/details?id=com.easy.counter">Play Store</a><br><br>
+                    <div class="col-6">
+                    <img src="media/easy_counter_logo512.png" width="128" height="128">
+                    </div>
+                </p>
+            </div>
+        <div class="row">
+            <div class="block-second col-6">
+                <h2 class="block-title">Video</h2>
+                <p class="col-6">
+                    cose magiche
+                </p>
+            </div>
+            <div class="block-first col-6">
+                <h2 class="block-title">Random stuff</h2>
+                <p class="col-6">
+                    Cose random
+                </p>
+            </div>
+        </div>
+        
+
+        <footer>
+            Copyright © 2021 Greedy Spaghetti
+        </footer>
+        
+    </body>
 </html>
---
+++ utils/header.php
@@ -0,0 +1,3 @@
+<meta name="viewpoint" content="width=device-width, initial-scale=1.0">   
+<link type="text/css" rel="stylesheet" href="./css/nice.css">
+<meta charset="UTF-8">

a65223374faf6646afefbcd76ed99e73a973bab6
samuele <samuelebompani@gmail.com>
2022-10-14 11:19:09 +0200

    feat: navbar in tokens

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,9 +1,12 @@
 export default function NavbarItem(props: {name: string, href: string}): JSX.Element {
     return <>
-        <div className="text-3xl lg:text-2xl font-bold text-rich_black p-5 
-                    hover:text-rich_black hover:bg-secondary rounded-2xl hover:animate-wiggle
-                    cursor-pointer">
-            <a href={props.href}>{props.name}</a> 
+        <div className=" border-solid border-black border-t-0 border-l-0 border-r-4 border-b-4 m-1 rounded-lg bg-black">
+            <div className="text-3xl lg:text-2xl font-bold text-rich_black p-5 
+                        hover:text-rich_black hover:bg-secondary rounded-md hover:animate-wiggle
+                        cursor-pointer border-solid border-2 border-black bg-primary">
+                <a href={props.href}>{props.name}</a> 
+            </div>
         </div>
+        
     </>
 }

ab2da324f92e00a0a2690ce408efbb3094964575
samuelebompani <samuelebompani@gmail.com>
2023-07-18 23:17:07 +0200

    burger menu

--- components/header.php
+++ components/header.php
@@ -1,3 +1,30 @@
+<script type="text/javascript">
+    function openCloseMenu() {
+        var x = document.getElementById("burger-menu");
+        var btnTop = document.getElementById("header-btn-top");
+        var btnCenter = document.getElementById("header-btn-center");
+        var btnBottom = document.getElementById("header-btn-bottom");
+        var menu = document.querySelector(".header-btn");
+        if (x.style.display === "flex") {
+            x.style.display = "none";
+            btnCenter.style.visibility = "visible";
+            btnCenter.style.opacity = "100";
+            btnTop.style.rotate = "0deg";
+            btnTop.style.marginBottom = "4px";
+            btnBottom.style.rotate = "0deg";
+            btnBottom.style.marginTop = "4px";
+        } else {
+            x.style.display = "flex";
+            btnCenter.style.visibility = "hidden";
+            btnCenter.style.opacity = "0";
+            btnTop.style.rotate = "45deg";
+            btnTop.style.marginBottom = "-2px";
+            menu.style.gap = "0px";
+            btnBottom.style.rotate = "-45deg";
+            btnBottom.style.marginTop = "-2px";
+        }
+    }
+</script>
 <div class="header">
     <div class="header-title boxed">
         Greedy Spaghetti
@@ -7,10 +34,14 @@
         <a href="/random" class="boxed">random</a>
         <a href="/about" class="boxed">about</a>
     </div>
-    <div class="header-btn boxed">
-        <div></div>
-        <div></div>
-        <div></div>
-
+    <div class="header-btn boxed" onClick="openCloseMenu()">
+        <div id="header-btn-top"></div>
+        <div id="header-btn-center"></div>
+        <div id="header-btn-bottom"></div>
     </div>
+</div>
+<div class="burger-menu" id="burger-menu">
+    <a href="/" class="boxed">home</a>
+    <a href="/random" class="boxed">random</a>
+    <a href="/about" class="boxed">about</a>
 </div>

85ec85b00e4b21de0a01be750fc03194da0b291d
samuelebompani <samuelebompani@gmail.com>
2023-07-18 23:35:28 +0200

    boxes in home

--- index.php
+++ index.php
@@ -4,6 +4,19 @@
     </head>
     <body>
         <?php include "./components/header.php"; ?>
-        
+        <div class="page">
+            <div class="grid">
+                <div class="boxed grid-item">
+                    <h2>Random</h2>
+                    <p>Some random stuff</p>
+                </div>
+                <div class="boxed grid-item">
+                    <h2>Easy Counter</h2>
+                    <p>If you need to count something, <br>here's your app.<br>
+                        Download available in the Play Store.
+                    </p>
+                </div>
+            </div>
+        </div>
     </body>
 </html>

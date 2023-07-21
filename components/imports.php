38b6bcb05e7bcf40dabb80a7abee36d48e9d1eea
samuelebompani <samuelebompani@gmail.com>
2023-07-21 15:53:13 +0200

    added spaghetto

--- components/imports.php
+++ components/imports.php
@@ -5,5 +5,5 @@
 <link rel="icon" type="image/x-icon" href="/media/img/favicon.ico">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap,wght@50..200,200..900');
 </style>

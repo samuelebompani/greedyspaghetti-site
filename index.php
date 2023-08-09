2d1900f59786482abb5d05e8ba1f999331bc0eba
samuelebompani <samuelebompani@gmail.com>
2023-08-09 18:29:36 +0200

    added some spaghetti

--- index.php
+++ index.php
@@ -6,7 +6,16 @@
     <body>
         <?php include "./components/header.php"; ?>
         <div class="spaghetti">
-            <div class="spaghetto"></div>
+            <div class="spaghetto" id="s1"></div>
+            <div class="spaghetto" id="s2"></div>
+            <div class="spaghetto" id="s3"></div>
+            <div class="spaghetto" id="s4"></div>
+            <div class="spaghetto" id="s5"></div>
+            <div class="spaghetto" id="s6"></div>
+            <div class="spaghetto" id="s7"></div>
+            <div class="spaghetto" id="s8"></div>
+            <div class="spaghetto" id="s9"></div>
+            <div class="spaghetto" id="s10"></div>
         </div>
         <div class="home-page" onClick="onTitleClick()">
             
@@ -17,12 +26,30 @@
     </body>
     <script type="text/javascript">
         function addSpaghetti() {
-            const newSpaghetto = document.createElement("div");
-            const spaghetti = document.querySelector(".spaghetti");
-            newSpaghetto.class = "spaghetto";
-            spaghetti.addChildren(newSpaghetto);
-            newSpaghetto.style.rotate = "30deg";
+            const s1 = document.getElementById("s1");
+            const s2 = document.querySelector("#s2");
+            const s3 = document.getElementById("s3");
+            const s4 = document.querySelector("#s4");
+            const s5 = document.getElementById("s5");
+            const s6 = document.getElementById("s6");
+            const s7 = document.querySelector("#s7");
+            const s8 = document.getElementById("s8");
+            const s9 = document.querySelector("#s9");
+            const s10 = document.getElementById("s10");
+            s1.style.marginLeft = "8px";
+            s2.style.marginLeft = "4px";
+            s3.style.marginLeft = "12px";
+            s4.style.marginLeft = "16px";
+            s5.style.marginLeft = "20px";
+            s6.style.marginLeft = "24px";
+            s7.style.marginLeft = "28px";
+            s8.style.marginLeft = "32px";
+            s9.style.marginLeft = "36px";
+            s10.style.marginLeft = "40px";
+            
         }
+
+        addSpaghetti();
 
         function onTitleClick() {
             var menu = document.querySelector(".header");

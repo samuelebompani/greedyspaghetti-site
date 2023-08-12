f54db199327a02144c31a5cbb6451425c5e93738
samuelebompani <samuelebompani@gmail.com>
2023-08-12 17:33:42 +0200

    random color

--- random.php
+++ random.php
@@ -15,9 +15,18 @@
                     +x+"%2C45."+y+"&amp;layer=mapnik"
                 console.log(x,y)
             }
+
+            function newColor() {
+                let r = getRandomArbitrary(0, 250)
+                let g = getRandomArbitrary(0, 250)
+                let b = getRandomArbitrary(0, 250)
+                var color = document.getElementById("color-box");
+                color.style = "background-color: rgb("+r+","+g+","+b+");"
+            }
             
             window.onload = function() {
                 newRandomPlace()
+                newColor()
             };
         </script>
     </head>
@@ -29,11 +38,25 @@
                     <h2>Random place in Brianza</h2>
                     <div style="width: 100%;">
                         <div style="width: 100%;">
-                            <iframe id="brianza-iframe" class="iframe-boxed"
-                                >
+                            <iframe id="brianza-iframe" class="iframe-boxed">
                             </iframe>
                             <div class="grid-item-btns">
                                 <div class="boxed wide-btn" onClick="newRandomPlace()">
+                                    Refresh    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="boxed grid-item">
+                    <h2>Random color</h2>
+                    <div style="width: 100%;">
+                        <div style="width: 100%;">
+                            <div id="color-box" class="iframe-boxed">
+                            </div>
+                            <div class="grid-item-btns">
+                                <div class="boxed wide-btn" onClick="newColor()"
+                                    style="margin-top: 5px;">
                                     Refresh    
                                 </div>
                             </div>

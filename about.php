f0d1ee89f365090399980b8c68adf60fa0425189
Samuele Bompani <samuelebompani@gmail.com>
2024-03-03 12:38:01 +0100

    about me skeleton

--- about.php
+++ about.php
@@ -6,9 +6,17 @@
         <?php include "./components/header.php"; ?>
         <div class="page">
             <div class="boxed grid-item">
-                <h2>Page under deployment</h2>
+                <h2>About me</h2>
                 <div>
-                    <img src="/media/img/wip.png" width=100 height=100 class=""/>
+                    Hello! I'm Samuele and I'm a developer. This is my personal site where I publish all my projects.<br>
+                    Some of them are just ideas and experiments that I abandon after a while, 
+                    some are projects I care more about. <br>Explore and enjoy!
+                    
+                </div>
+                <div class="marginy-10">
+                    <a href="https://github.com/samuelebompani">
+                        <img src="/media/img/github-mark.svg" width=50 height=50 class=""/>
+                    </a>
                 </div>
             </div>     
         </div>

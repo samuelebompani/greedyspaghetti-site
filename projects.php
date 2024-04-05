07f5c683e40e232edc7c771a4fe0f6deed215145
Samuele Bompani <samuelebompani@gmail.com>
2024-04-05 17:31:07 +0200

    football headers + apk for easy counter

--- projects.php
+++ projects.php
@@ -1,91 +1,112 @@
 <!DOCTYPE html>
-    <head>
-        <?php include "./components/imports.php"; ?>
-    </head>
-    <body>
-        <?php include "./components/header.php"; ?>
-        <div class="page">
-            <div class="grid">
-                <div class="boxed grid-item">
-                    <h2>Easy Counter</h2>
-                    <div class="justify-between w-full">
-                        <img src="/media/img/easy_counter_logo.png" width=100 height=100 class=""/>
-                        <div class="w-full">
-                            <p>If you need to count something, <br>here's your app.<br>
-                                    Download available in the Play Store.
-                            </p>
-                            <div class="grid-item-btns">
-                                <a href="https://play.google.com/store/apps/details?id=com.easy.counter&gl=US"
-                                    class="boxed square-btn">
-                                    <img src="/media/img/playstore.png" width=50 height=50/>
-                                </a>
-                                <div class="boxed square-btn">
-                                    <img src="/media/img/info.png" width=50 height=50/>
-                                </div>   
+
+<head>
+    <?php include "./components/imports.php"; ?>
+</head>
+
+<body>
+    <?php include "./components/header.php"; ?>
+    <div class="page">
+        <div class="grid">
+            <div class="boxed grid-item">
+                <h2>Easy Counter</h2>
+                <div class="justify-between w-full">
+                    <img src="/media/img/easy_counter_logo.png" width=100 height=100 class="" />
+                    <div class="w-full">
+                        <p>If you just need to count something, <br>here's your app.<br><br>
+                            
+                        </p>
+                        <div class="grid-item-btns">
+                            <a href="/apps/EasyCounter.apk"
+                                class="boxed square-btn">
+                                <img src="/media/img/arrow.png" width=50 height=50 />
+                            </a>
+                            <div class="boxed square-btn">
+                                <img src="/media/img/info.png" width=50 height=50 />
                             </div>
                         </div>
                     </div>
                 </div>
-                <?php /*<div class="boxed grid-item">
-                    <h2>Football stats</h2>
-                    <div>
-                        <img src="/media/img/fs1.png" width=100 height=100 class=""/>
-                        <div>
-                            <p>A collection of football stats, starting<br> from the 2023/2024
-                                    season.<br><br>
-                            </p>
-                            <div class="grid-item-btns">
-                                <a href="/fs/"
-                                    class="boxed square-btn">
-                                    <img src="/media/img/arrow.png" width=50 height=50/>
-                                </a>
-                                <div class="boxed square-btn">
-                                    <img src="/media/img/info.png" width=50 height=50/>
-                                </div>   
-                            </div>
-                        </div>
-                    </div>
-                </div>*/ ?>
-                <div class="boxed grid-item">
-                    <h2>Recipes</h2>
-                    <div class="justify-between w-full">
-                        <img src="/media/img/r.png" width=100 height=100 class=""/>
-                        <div class="w-full">
-                            <p>A collection of my favourite recipes. <br><br><br>
-                            </p>
-                            <div class="grid-item-btns">
-                                <a href="/recipes/"
-                                    class="boxed square-btn">
-                                    <img src="/media/img/arrow.png" width=50 height=50/>
-                                </a>
-                                <div class="boxed square-btn">
-                                    <img src="/media/img/info.png" width=50 height=50/>
-                                </div>   
+            </div>
+            <?php /*<div class="boxed grid-item">
+               <h2>Football stats</h2>
+               <div>
+                   <img src="/media/img/fs1.png" width=100 height=100 class=""/>
+                   <div>
+                       <p>A collection of football stats, starting<br> from the 2023/2024
+                               season.<br><br>
+                       </p>
+                       <div class="grid-item-btns">
+                           <a href="/fs/"
+                               class="boxed square-btn">
+                               <img src="/media/img/arrow.png" width=50 height=50/>
+                           </a>
+                           <div class="boxed square-btn">
+                               <img src="/media/img/info.png" width=50 height=50/>
+                           </div>   
+                       </div>
+                   </div>
+               </div>
+           </div>*/ ?>
+            <div class="boxed grid-item">
+                <h2>Recipes</h2>
+                <div class="justify-between w-full">
+                    <img src="/media/img/r.png" width=100 height=100 class="" />
+                    <div class="w-full">
+                        <p>A collection of my favourite recipes. <br><br><br>
+                        </p>
+                        <div class="grid-item-btns">
+                            <a href="/recipes/" class="boxed square-btn">
+                                <img src="/media/img/arrow.png" width=50 height=50 />
+                            </a>
+                            <div class="boxed square-btn">
+                                <img src="/media/img/info.png" width=50 height=50 />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="boxed grid-item">
-                    <h2>A quick BBBreak</h2>
-                    <div class="justify-between w-full">
-                        <img src="/media/img/bbblogo.jpg" width=100 height=100 class=""/>
-                        <div class="w-full">
-                            <p>A newsletter I write with my friends. It's in italian and we send a new mail every month. You can subscribe clicking here<br>
-                            </p>
-                            <div class="grid-item-btns">
-                                <a href="http://eepurl.com/hOhaeX"
-                                    class="boxed square-btn">
-                                    <img src="/media/img/arrow.png" width=50 height=50/>
-                                </a>
-                                <div class="boxed square-btn">
-                                    <img src="/media/img/info.png" width=50 height=50/>
-                                </div>   
+            </div>
+            <div class="boxed grid-item">
+                <h2>A quick BBBreak</h2>
+                <div class="justify-between w-full">
+                    <img src="/media/img/bbblogo.jpg" width=100 height=100 class="" />
+                    <div class="w-full">
+                        <p>A newsletter I write with my friends. It's in italian and we send a new mail every month. You
+                            can subscribe clicking here<br>
+                        </p>
+                        <div class="grid-item-btns">
+                            <a href="http://eepurl.com/hOhaeX" class="boxed square-btn">
+                                <img src="/media/img/arrow.png" width=50 height=50 />
+                            </a>
+                            <div class="boxed square-btn">
+                                <img src="/media/img/info.png" width=50 height=50 />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="boxed grid-item">
+                <h2>Football Headers</h2>
+                <div class="justify-between w-full">
+                    <img src="/media/img/icona_FH_play.png" width=100 height=100 class="" />
+                    <div class="w-full">
+                        <p>A game for Android I developed in 2018. Control the ball with your head, collect coins to
+                            unlock new players and backgrounds.<br>
+                        </p>
+                        <div class="grid-item-btns">
+                            <a href="/apps/FootballHeaders.apk" class="boxed square-btn" download>
+                                <img src="/media/img/arrow.png" width=50 height=50 />
+                            </a>
+                            <div class="boxed square-btn">
+                                <img src="/media/img/info.png" width=50 height=50 />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <?php include "./components/footer.php"; ?>
-    </body>
+    </div>
+    <?php include "./components/footer.php"; ?>
+</body>
+
 </html>

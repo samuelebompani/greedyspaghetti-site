df87f67153a92aa2d54d83a1d5a4c086d444e00e
Samuele Bompani <samuelebompani@gmail.com>
2024-09-01 14:45:13 +0200

    page elements

---
+++ norimberga/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/header.css"> 
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="../css/recipes.css">
+        <link rel="stylesheet" href="../css/norimberga.css">
+        <?php include "../components/imports.php";?>
+        <script type="text/javascript">
+            
+        </script>
+        
+    </head>
+    <body>
+        <?php include "../components/header.php"; ?>
+        <div class="page">
+            <div class="grid" style="grid-template-columns: repeat(1, minmax(0, 1fr));">
+                <div class="boxed grid-item">
+                    <div class="boxed bg-secondary title-menu-el-recipes">Norimberga</div>
+                    <div class="boxed bg-secondary word-nor-box">Palomino</div>
+                    <div class="grid-item-btns">
+                        <div class="boxed wide-btn " style="padding-right: 0">Refresh</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "../components/footer.php"; ?>
+    </body>
+</html>
--- projects.php
+++ projects.php
@@ -104,6 +104,26 @@
                     </div>
                 </div>
             </div>
+            <div class="boxed grid-item">
+                <h2>Norimberga</h2>
+                <div class="justify-between w-full">
+                    <img src="/media/img/easy_counter_logo.png" width=100 height=100 class="" />
+                    <div class="w-full">
+                        <p><br><br><br>
+                            
+                        </p>
+                        <div class="grid-item-btns">
+                            <a href="/norimberga/"
+                                class="boxed square-btn">
+                                <img src="/media/img/arrow.png" width=50 height=50 />
+                            </a>
+                            <div class="boxed square-btn">
+                                <img src="/media/img/info.png" width=50 height=50 />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <?php include "./components/footer.php"; ?>
